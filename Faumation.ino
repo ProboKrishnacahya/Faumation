f655406cd7dec06c2a9c14a8dd93980742b779a2
Probo Krishnacahya <probo.krishnacahya@gmail.com>
2022-05-26 01:12:08 +0700

    cicil otomasi pengisian & pemanas air

--- Faumation.ino
+++ Faumation.ino
@@ -1,20 +1,25 @@
-#include "DHT.h"
-#define DHTTYPE DHT22
-#define DHTPin 8
+#include "DHT.h" // Library Sensor Suhu DHT
+#define DHTTYPE DHT11 // Menggunakan Sensor Suhu tipe DHT11
+#define DHTPin 8 // Digital Pin Arduino yang dihubungkan ke Pin Data dari Sensor Suhu DHT11
 
 int pinPemanas = 10; // Jika LED Red menyala maka pemanas air sedang menyala, jika LED Red mati maka pemanas air sedang mati
 int pinButtonPemanas = 9;
 bool statusPemanas = false; // Jika false maka pemanas air mati, tapi jika true maka pemanas air menyala
 //int pinSensorSuhu = 8;
+int pinTrigger = 12; // Pin Trigger Sensor Jarak Ultrasonic
+int pinEcho = 11; // Pin Echo Sensor Jarak Ultrasonic
+long duration, cm; // Jarak Ketinggian Batas Atas dan Batas Bawah Tempat Pengisian Air
+int M1 = 13; // Motor DC
 
-DHT dht(DHTPin, DHTTYPE);
+DHT dht(DHTPin, DHTTYPE); // Object Sensor Suhu DHT11
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(pinButtonPemanas, INPUT_PULLUP);
   pinMode(pinPemanas, OUTPUT);
+  pinMode(M1, OUTPUT);
   // digitalWrite(pinButtonPemanas, HIGH); // Untuk menyalakan INPUT_PULLUP sehingga secara default kondisinya HIGH
-  Serial.begin(9600);
+  Serial.begin(9600); // Baud Rate Serial Monitor
   dht.begin();
 }
 
@@ -25,10 +30,62 @@ void loop() {
     delay(20);
   }
   digitalWrite(pinPemanas, statusPemanas);
-  delay(1000);
+  delay(100);
 
   float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  Serial.println(t);
-  Serial.println(h);
+  int i;
+  i = (int) t;
+  if (isnan(t)) {
+    Serial.println("Gagal membaca data.");
+  } else {
+    Serial.print(F("Temperatur: "));
+    Serial.println(i);
+    if (i <= 20) {
+      Serial.println("Pemanas Air Menyala.");    
+    } 
+    if (i >= 40) {
+      Serial.println("Pemanas Air Mati.");    
+    }    
+  }
+  delay(500);  
+
+  pinMode(pinTrigger, OUTPUT);
+  digitalWrite(pinTrigger, LOW); // Melakukan Trigger dengan Pulse
+  delayMicroseconds(2);
+  digitalWrite(pinTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrigger, LOW);
+
+  pinMode(pinEcho, INPUT);
+  duration = pulseIn(pinEcho, HIGH);
+  cm = durationToCentimeter(duration);
+  Serial.print(F("Kapasitas Air: "));
+  Serial.print(cm);
+  Serial.println(" %");
+  if (cm <= 10) {
+    Serial.println("Saluran Air Menyala.");    
+  } 
+  if (cm >= 90) {
+    Serial.println("Saluran Air Mati.");    
+  }
+  delay(100);
+
+  if (Serial.available()) {
+    char data = Serial.read();
+    Serial.print(F("Arah: "));
+    Serial.println(data);
+    if (data == 'A') {
+      digitalWrite(M1, LOW);
+    }
+    if (data == 'C') {
+      digitalWrite(M1, LOW);
+    }
+    if (data == 'S') {
+      digitalWrite(M1, HIGH);
+    }    
+  }
+}
+
+long durationToCentimeter(long ns) {
+  return ns / 29 / 2;
 }
---
+++ README.md
@@ -0,0 +1,21 @@
+# Faumation
+
+> Assesment for Learning 4 Internet of Things 2021 - 2022 Genap (Semester 4) Informatika Universitas Ciputra Surabaya.
+
+### 👨‍💻👩‍💻 Tentang Pengembang
+
+Kelompok 2:
+
+- 0706012010013 - Nur Azizah
+- 0706012010029 - Nathanael Abel Arianto
+- 0706012010039 - Probo Krishnacahya
+- 0706012010055 - Haning Galih Rani Kumbara
+
+### 📱 Tentang Aplikasi
+
+Faumation adalah proyek yang dibuat menggunakan teknologi Internet of Things dengan topik "Sistem Pemantauan Ketinggian Air dan Kontrol Suhu Air dengan Otomasi Keran Berbasis Internet of Things Melalui Web Menggunakan Modul Arduino Uno R3 V2.0 Pada Penampungan Air Warga". Faumation tersedia dalam bentuk simulasi [Proteus](https://www.labcenter.com/) maupun simulasi dalam bentuk Website Faumation yang dapat diakses melalui Web Browser.
+
+Tujuan pembuatan Faumation:
+
+- [x] Memudahkan warga dalam proses pengisian air pada tempat pengisian air mandi yang mana saluran airnya dapat dijalankan secara otomasi dengan deteksi jarak batas atas dan bawahnya maupun opsi pengisian yang dikendalikan secara manual sehingga dapat menghemat waktu dan tenaga sekaligus mencegah pemakaian air mandi yang terbuang percuma karena melewati kapasitas tempat pengisian air ketika sedang melakukan pengisian air.
+- [x] Memberikan kenyamanan melalui suhu air yang dapat disesuaikan dengan kebutuhan warga, yang mana tersedia pendeteksi suhu air yang dapat dijalankan secara otomasi (jika mencapai suhu < 20&deg;C, maka pemanas air akan menyala sehingga warga bisa mendapatkan air dengan suhu hangat dan ketika mencapai 40&deg;C, maka pemanas air akan mati). Sedangkan, jika warga menginginkan suhu air >40&deg;C, maka warga dapat menggunakan opsi pengendalian pemanas air secara manual untuk penetapan suhunya.
